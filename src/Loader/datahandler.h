9cc3daa65eed53052b7fe56b950e853e1069418d
Testelair <demarets.maxime.jimmy@gmail.com>
2025-01-26 14:59:58 +0100

    Updated datahandler.h

---
+++ src/Loader/datahandler.h
@@ -0,0 +1,55 @@
+#ifndef DATAHANDLER_H
+#define DATAHANDLER_H
+
+#include <cstdint>
+#include <cstdio>
+#include "level.h"
+
+struct DataHeader
+{
+    uint32_t    GlobalObjGroup;
+    uint32_t    GlobalSprites;
+    uint32_t    GlobalSpritesCopy;    // Safety Copy of GlobalSprites
+    uint32_t    GlobalStrings;
+    uint32_t    GlobalStringsCopy;    // Safety Copy of GlobalStrings
+    uint32_t    Unknown;              // Probably flags. Composed of 96 blocks of 2048 bytes
+    uint32_t    LoadingScreen0;       // 320 width images
+    uint32_t    LoadingScreen1;
+    uint32_t    LoadingScreen2;
+    uint32_t    LoadingScreen3;
+    uint32_t    Levels[484];          // Pointer 483 is EndOfFile
+    uint32_t    NullPointers[18];
+};
+
+class DataHandler
+{
+    private:
+
+    uint32_t     _fileSize;
+    FILE*        _dataFile;
+    DataHeader*  _dataHeader;
+    char*        _globalData;
+    uint32_t     _globalSize;
+    Level*       _level;
+
+    public:
+
+    /// Constructor
+    DataHandler();
+
+    /// Reads to DATAS.BIN and returns 0 if succeed
+    int ReadFile(const char* dataPath);
+    /// Loads global data and returns 0 if succeed
+    int LoadGlobal();
+    /// Loads a level and returns 0 if succeed
+    int LoadLevel(uint16_t dex);
+
+    // Getters
+
+    char* GetGlobPtr()  { return _globalData; }
+
+    /// Destructor
+    ~DataHandler();
+};
+
+#endif // DATAHANDLER_H
